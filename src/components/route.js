1a91845bfa813cfa39c23c6f6bd16ca6b8c34433
adityasrivast <yashsri421@gmail.com>
2018-11-03 12:27:07 +0530

    add dashboard

--- src/components/route.js
+++ src/components/route.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Dashboard from './dashboard';
 
 class Router extends Component {
     render() {
         return(
             <BrowserRouter>
-                
+                <Switch>
+                    <Route exact path='/dashboard' component={Dashboard}/>
+                </Switch>
             </BrowserRouter>
         );
     }
